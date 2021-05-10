5128981fa138254853c0817ed8170d8fa1a5e4d0
lawrencelucky <lawrencelucky1999@gmail.com>
2021-05-11 00:32:43 +0100

    Adjusted my DB

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ require('dotenv').config({ path: './config.env' });
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
   .connect(DB, {
