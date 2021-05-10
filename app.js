45de0a02035a2e01372a2a5111f2447f77f40b26
lawrencelucky <lawrencelucky1999@gmail.com>
2021-05-11 00:05:30 +0100

    made some adjustment to routing

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send(`<h3>NodeJs Task 3</h3>`);
-});
+app.get('/', (req, res) => res.send(`<h3>NodeJs Task 3</h3>`));
 
 app.use('/api/bio', bioRoutes);
 
